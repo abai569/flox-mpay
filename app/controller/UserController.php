7dba28ecc83921184ead5a1926b464fe47ecfbe0
abai569 <abaibubaix@gmail.com>
2026-06-10 10:54:47 +0800

    fix: 修复菜单接口返回 null 问题
    改为直接 include 配置文件，避免 config() 和 Config::load() 的解析问题

--- app/controller/UserController.php
+++ app/controller/UserController.php
@@ -58,7 +58,7 @@ class UserController extends BaseController
     // 管理菜单
     public function menu()
     {
-        $menu = config('extend.menu');
+        $menu = include config_path('extend/menu.php');
         return json($menu);
     }
 }
