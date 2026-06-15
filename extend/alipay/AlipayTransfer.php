00717880b5e8d079ce9be4106ca855c7ef66a828
abai569 <abaibubaix@gmail.com>
2026-06-15 12:31:36 +0800

    fix: AlipayTransfer 改用直接 alipays:// 链接，去掉 render.alipay.com 层套

--- extend/alipay/AlipayTransfer.php
+++ extend/alipay/AlipayTransfer.php
@@ -25,33 +25,15 @@ class AlipayTransfer
             throw new \RuntimeException('未配置支付宝用户ID (transfer_user_id)');
         }
 
-        $outerAppId = '20000218';
-        $innerAppId = '20000116';
-        $mdeductUrl = 'https://render.alipay.com/p/c/mdeduct-landing';
-        $renderUrl = 'https://render.alipay.com/p/s/i';
-
-        // 最内层：转账参数
-        $innerParams = [
-            'appId'      => $innerAppId,
+        $params = [
+            'appId'      => '20000116',
             'actionType' => 'toAccount',
             'goBack'     => 'NO',
             'amount'     => number_format($amount, 2, '.', ''),
             'userId'     => $userId,
             'memo'       => $memo,
         ];
-        $innerUrl = 'alipays://platformapi/startapp?' . http_build_query($innerParams);
 
-        // 第四层
-        $fourthUrl = $renderUrl . '?scheme=' . urlencode($innerUrl);
-
-        // 第三层
-        $thirdParams = ['appId' => $outerAppId, 'url' => $fourthUrl];
-        $thirdUrl = 'alipays://platformapi/startapp?' . http_build_query($thirdParams);
-
-        // 第二层
-        $secondUrl = $renderUrl . '?scheme=' . urlencode($thirdUrl);
-
-        // 最外层
-        return $mdeductUrl . '?scheme=' . urlencode($secondUrl);
+        return 'alipays://platformapi/startapp?' . http_build_query($params);
     }
 }
